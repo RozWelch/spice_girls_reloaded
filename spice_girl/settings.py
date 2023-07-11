15c239f8d705846cb93b5f953a68ba993391bd19
RozWelch <rozwelch@gmail.com>
2023-07-11 11:05:25 +0000

    Updated database settings

--- spice_girl/settings.py
+++ spice_girl/settings.py
@@ -87,19 +87,19 @@ WSGI_APPLICATION = 'spice_girl.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    # Use the deployed db stored in heroku config vars
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
+# if 'DATABASE_URL' in os.environ:
+#     # Use the deployed db stored in heroku config vars
+#     DATABASES = {
+#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#     }
+# else:
     # use the development sqlite db
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
-    } 
+        }
 
 
 # Password validation
