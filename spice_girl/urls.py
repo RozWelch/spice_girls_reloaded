269fe1114757cf35bfaf04eb13c970a362df3edc
IvetteMcDermott <ivettedemcdermott@gmail.com>
2023-07-13 13:07:58 +0000

    RecipesApp: Change for flow recipes to recipes_app

--- spice_girl/urls.py
+++ spice_girl/urls.py
@@ -21,9 +21,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('recipes/', include('recipes_app.urls'), name='recipes'),
+    path('recipes/', include('recipes_app.urls')),
     path('products/', include('products.urls')),
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
